f318d23723546f3d5e2dee7456005e619b025f76
Adam Dias <adam.dias@outlook.com>
2019-10-11 17:18:08 -0300

    add gitignore

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,35 +1,35 @@
-const express = require('express'); 
+const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user'); 
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-// importando o secretHash 
+// importando o secretHash
 const authConfig = require('../config/auth.json');
 
-const router = express.Router(); 
+const router = express.Router();
 
-// function geradora de token's 
+// function geradora de token's
 function generateToken(params = {}) {
-    // gerando token 
+    // gerando token
     return jwt.sign(params, authConfig.secret, {
         expiresIn: 86400,
-    }); 
+    });
 }
 
-// rota de Registro de usuário 
+// rota de Registro de usuário
 router.post('/register', async (req, res) => {
-    const { email } = req.body; 
+    const { email } = req.body;
 
     try {
         if(await User.findOne({ email }))
-            return res.status(400).send({error: 'User already exists'}); 
+            return res.status(400).send({error: 'User already exists'});
 
         const user = await User.create(req.body);
 
-        user.password = undefined; 
-                
+        user.password = undefined;
+
         return res.send({
-            user, 
+            user,
             token: generateToken({ id: user.id }),
         });
 
@@ -40,26 +40,26 @@ router.post('/register', async (req, res) => {
 
 // rota de Autenticação
 router.post('/authenticate', async (req, res) =>{
-    const { email, password } = req.body; 
+    const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select('+password'); 
-    
+    const user = await User.findOne({ email }).select('+password');
+
     if(!user){
         res.status(400).send({error: 'User not found'});
     }
-    
-    // comparação de hash's 
+
+    // comparação de hash's
     if(!await bcrypt.compare(password, user.password))
         return res.status(400).send({error: 'Invalid password'});
-        
-        //removendo o password da resposta 
-        user.password = undefined; 
+
+        //removendo o password da resposta
+        user.password = undefined;
 
         return res.send({
-             user, 
-             token: generateToken({ id: user.id }) 
+             user,
+             token: generateToken({ id: user.id })
         });
-    
-}); 
 
-module.exports = app => app.use('/auth', router); 
+});
+
+module.exports = app => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const express = require('express'); 
-const mongoose = require('mongoose'); 
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express(); 
+const app = express();
 
 mongoose.set('useCreateIndex', true);
+
 mongoose.connect(
     'mongodb://localhost:27017/noderest',
-    {   useNewUrlParser: true,
+    {
+        useNewUrlParser: true,
         useUnifiedTopology: true,
     }
 );
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-require('./controllers/authController')(app); 
-require('./controllers/projectController')(app); 
-require('./controllers/testController')(app); 
+require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
+require('./controllers/testController')(app);
 
-
-app.listen(3001); 
+app.listen(3001);
